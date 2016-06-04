7db8fc41b991c6128a4a17eeb9d7a888a483fa7c
Philipdev95 <philipdev@icloud.com>
2016-06-04 17:22:57 +0200

    lite småfix
    Javascript Fix med SHOWHIDEMENU()

--- main.js
+++ main.js
@@ -6,11 +6,14 @@ window.onload = loading();
 function openCloseMenu() {
     
     var stangMeny = document.getElementById("closemeny"),
+        openMeny = document.getElementById("openmeny"),
         meny = document.getElementById("meny");
     
     if (meny.style.display == "none") {
         meny.style.display = "block";
+        openMeny.style.display = "none";
     } else {
         meny.style.display = "none";
+        openMeny.style.display = "block";
     }
 }
